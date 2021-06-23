3519ee31d0d5077fbb2cfe19cc19d6f94a889fae
thenx6 <adrianokisssu@gmail.com>
2021-06-23 14:32:04 +0200

    jquery added
    Jquery Added

--- pasztor/js/app.js
+++ pasztor/js/app.js
@@ -1,11 +1,12 @@
-$("document").ready(function(){
-    $(".navtoggler").on("click",function(){
-        $(".togglenav").toggle(
-            function(){
-                $(this).addClass("hide");
-            },function(){
-                $(this).removeClass("hide");
-            }
-        )
-    });
+$("document").ready(function () {
+  $(".navtoggler").on("click", function () {
+    $(".togglenav").removeClass("hide"), $(".navtogglerx").removeClass("hide");
+    $(".navtoggler").addClass("hide");
+  });
+
+  $(".navtogglerx").on("click", function () {
+    $(".togglenav").addClass("hide"),
+      $(".navtoggler").removeClass("hide"),
+      $(".navtogglerx").addClass("hide");
+  });
 });
